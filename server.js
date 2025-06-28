4ab91d9146d088fa6adf9ec80520a432410fedcc
Aditya Kumar Singh <ad69832@gmail.com>
2025-06-28 15:10:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const apiRoutes = require("./routes/api");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use("/api", apiRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
