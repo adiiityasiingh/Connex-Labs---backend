b46affe2857e93af7271bba7315f75285c35e220
Aditya Kumar Singh <ad69832@gmail.com>
2025-06-28 15:09:45 +0530

    Update app.py

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify
 from datetime import datetime, timedelta
 import random
-
 app = Flask(__name__)
-
 @app.route('/predict-maintenance', methods=['POST'])
 def predict_maintenance():
     data = request.get_json()
